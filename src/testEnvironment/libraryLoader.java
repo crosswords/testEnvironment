91d2757405a3b835b5f80bbecd4f318d7f2e1018
TomekJ <jedynak1991@gmail.com>
2014-09-28 20:00:04 +0200

    zmiana ułożenia funkcji w klasach

--- src/testEnvironment/libraryLoader.java
+++ src/testEnvironment/libraryLoader.java
@@ -1,26 +1,14 @@
 package testEnvironment;
 
-import java.io.File;
-
 /**
  * Created by Tomek on 2014-09-28.
  */
 public class libraryLoader {
+    //public/private library ...
     public libraryLoader() {
 
     }
-    public void loadMultipleLibrary(String directory){
-        File file = new File(directory);
-        String[] names = file.list();
 
-        for(String name : names)
-        {
-            if (! new File(directory + "\\" + name).isDirectory())
-            {
-                loadLibrary(name);
-            }
-        }
-    }
     public void loadLibrary(String libName){
         // TO DO
     }
--- src/testEnvironment/testEnvironment.java
+++ src/testEnvironment/testEnvironment.java
@@ -1,11 +1,29 @@
 package testEnvironment;
 
+import java.io.File;
+
 /**
  * Created by Tomek on 2014-09-28.
  */
 public class testEnvironment {
-    public testEnvironment(){
+    public testEnvironment(/*int width,int height, args...*/){
 
     }
-    
+    public void testMultipleLibraries(String directory) {
+        File file = new File(directory);
+        String[] names = file.list();
+
+        for (String name : names) {
+            if (!new File(directory + "\\" + name).isDirectory()) {
+                testLibrary(name);
+            }
+        }
+    }
+    public void testLibrary(String name){
+        libraryLoader loader = new libraryLoader();
+
+        //async -> loader.library.generateCrossword(....);
+        //
+    }
+
 }
